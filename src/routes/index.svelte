dacff58e742fee75a1c85369c0d46553b541314f
zummon <zummon.space@gmail.com>
2022-06-20 00:25:00 +0700

    add tailwind css and buttons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,28 +24,34 @@
 		if (typeof option === 'object'){
 			if (option.date){
 				data = data.map((person) => {
-					let showname = person.personal.sort(compare).reduce((prev, cur) => {
+					let personal = person.personal.sort(compare).reduce((prev, cur) => {
 						if (cur.applydate > option.date){
 							return prev
 						}
 						return {...prev, ...cur}
 					}, datamodel.personal[0])
 
-					let showfield = person.field.sort(compare).reduce((prev, cur) => {
+					let field = person.field.sort(compare).reduce((prev, cur) => {
 						if (cur.applydate > option.date){
 							return prev
 						}
 						return {...prev, ...cur}
 					}, datamodel.field[0])
 					
-					return {...person, personal: showname, field: showfield}
+					return {...person, personal, field}
 				}).filter((person) => {
 					
 					return person.field.applydate <= option.date && person.field.position
 				})
 			} else if (option.id){
+				let obj = data.find(person => person.id === option.id)
+				obj = {...obj,
+					personal: obj.personal.sort(compare),
+					field: obj.field.sort(compare),
+					knowledge: obj.knowledge //.sort(compare)
+				}
 				
-				return data.find(person => person.id === option.id)
+				return obj
 			}
 			
 		}
@@ -53,6 +59,7 @@
 		return data;
 	}
 
+	let store = []
 	let date = now()
 	let promise = gather({
 		date: now()
@@ -62,31 +69,29 @@
 </script>
 
 <input bind:value={date} type='date' />
-<button on:click={() => {
+<button class="border px-3 py-1 font-semibold bg-orange-300" on:click={() => {
 	promise = gather({ date }) 
 }}>
 	Load
 </button>
 
-<hr>
-
 {#await promise}
 	<p>...waiting</p>
 {:then data}
-	<table>
+	<table class="border-b border-t">
 		<thead>
-			<tr>
-				<th>No</th>
-				<th>Name</th>
-				<th>Field</th>
+			<tr class="text-center border-b">
+				<td>No</td>
+				<td>Name</td>
+				<td>Field</td>
 			</tr>
 		</thead>
 		<tbody>
 			{#each data as obj, index (`data-${index}`)}
-				<tr on:click={async () => {
+				<tr class="hover:bg-orange-300 cursor-pointer {obj.id === person.id ? 'font-semibold' : ''}" on:click={async () => {
 					person = await gather({ id: obj.id })
 				}}>
-					<td>{index + 1}</td>
+					<td class="text-center">{index + 1}</td>
 					<td>
 						{obj.personal.beginwith} {obj.personal.firstname} {obj.personal.middlename} {obj.personal.lastname}
 					</td>
@@ -97,29 +102,57 @@
 			{:else}
 				not found...
 			{/each}
+			<tr>
+				<td></td>
+				<td>
+					<button class="border px-3 py-1 font-semibold bg-orange-300" on:click={() => {
+						//
+					}}>
+						Add
+					</button>
+				</td>
+				<td></td>
+			</tr>
 		</tbody>
 	</table>
 {/await}
 
-<hr>
+{#if person.id}
 
+<button class="border px-3 py-1 font-semibold bg-orange-300" on:click={() => {
+	person = {}
+}}>
+	Close
+</button>
 {#each Object.keys(person) as key, index (`person-${index}`)}
 	{#if typeof person[key] === 'string'}
-		<div style="display: flex; flex-wrap: wrap">
-			<div><b>{key}</b>:&nbsp;</div>
-			<div style="padding: 4px" contenteditable="true" bind:textContent={person[key]}></div>
+		<div class="flex flex-wrap">
+			<div class="">{key}:&nbsp;</div>
+			<div class="font-semibold" contenteditable="true" bind:textContent={person[key]}></div>
 		</div>
 	{:else if Array.isArray(person[key])}
-		<div style="margin: 8px 0 0 0">
-			<b>{key}</b>:
+		<div class="mt-4">
+			<span class="">{key}</span>:
+			<button class="border px-3 py-1 font-semibold bg-orange-300" on:click={() => {
+				//
+			}}>
+				Add
+			</button>
 			{#each person[key] as atr, ind (`atr-${index}-${ind}`)}
-				<div style="display: flex; flex-wrap: wrap; padding: 4px; border: 1px solid">
+				<div class="flex flex-wrap p-2 border">
 					{#each Object.keys(atr) as str, i (`key-${index}-${ind}-${i}`)}
-						<div><i>{str}</i>:&nbsp;</div>
-						<div style="margin: 0 8px 0 0; font-weight: 700" contenteditable="true" bind:textContent={person[key][ind][str]}></div>
+						<div class="">{str}:&nbsp;</div>
+						<div class="mr-4 font-semibold" contenteditable="true" bind:textContent={person[key][ind][str]}></div>
 					{/each}
+					<button class="border px-3 py-1 font-semibold bg-orange-300" on:click={() => {
+						//
+					}}>
+						Edit
+					</button>
 				</div>
 			{/each}
 		</div>
 	{/if}
 {/each}
+
+{/if}
